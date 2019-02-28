d28f3df062188384c21494c94345644cdd70ce80
viacheslav-pastushenko <slavpas@gmail.com>
2019-02-28 11:17:45 +0200

    Update

--- routes/register.js
+++ routes/register.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-
+  console.log(req.body);
+  // res.json({
+  //   ok: false
+  // })
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 const routes = require('./routes');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/build'));
 
 const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
@@ -12,13 +15,14 @@ mongoClient.connect((err, database) => {
     if(err) return err;
 
     app.locals.collection = database.db('users').collection('users');
-    app.listen(port, () => {
-        console.log('Started on port ' + port);
-    });
+});
+
+app.listen(port, () => {
+    console.log('Started on port ' + port);
 });
 
 app.get('/', (req, res) => {
     res.send('index');
 });
 
-//app.get('/api/user', routes.register)
+app.use('/api/user', routes.register);
